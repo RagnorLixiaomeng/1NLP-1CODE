374b91df5c6dd0a111686205fd2c556ebff6f754
RagnorLixiaomeng <ragnor.li@outlook.com>
2024-10-15 17:07:15 +0800

    half finish numpy 100 exercises

--- code/Stack&Queue/QueueSlideWindow.java
+++ code/Stack&Queue/QueueSlideWindow.java
@@ -5,7 +5,7 @@ import java.util.ArrayDeque;
  * 
  * 给定一个数组 nums，有一个大小为 k 的滑动窗口从数组的最左侧移动到数组的最右侧。你只可以看到在滑动窗口内的 k 个数字。滑动窗口每次只向右移动一位。返回滑动窗口中的最大值
  
- 输入：nums = [1, 3, -1, -3, 5, 3, 6, 7]，和 k = 3  
+输入：nums = [1, 3, -1, -3, 5, 3, 6, 7]，和 k = 3  
 输出：[3, 3, 5, 5, 6, 7]  
 解释：
 
@@ -22,7 +22,7 @@ import java.util.ArrayDeque;
 public class QueueSlideWindow {
 
      public static int[] maxSlidingWindow(int[] nums, int k) {
-        ArrayDeque<Integer> deque = new ArrayDeque<>();
+        ArrayDeque<Integer> deque = new ArrayDeque<>(); 
         int n = nums.length;
         int[] res = new int[n - k + 1];
         int idx = 0;
@@ -42,7 +42,7 @@ public class QueueSlideWindow {
             // 因为单调，当i增长到符合第一个k范围的时候，每滑动一步都将队列头节点放入结果就行了
             if(i >= k - 1){
                 res[idx++] = nums[deque.peek()];
-            }
+            } 
         }
         return res;
     }
